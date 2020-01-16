e74d50cba99d41b37f70a12fd8209bc4fdf93d53
nthmetal <832529@gmail.com>
2020-01-16 00:11:50 -0500

    discord communication

--- src/bot.ts
+++ src/bot.ts
@@ -1,3 +1,19 @@
-console.log('it works!');
+import * as Discord from 'discord.js';
+
+import * as auth from './config/auth.json';
+
+const gbot = new Discord.Client();
+
+gbot.on('ready', () => {
+    console.log(`Logged in as ${gbot.user.tag}!`);
+    gbot.user.setPresence({
+        status: 'online',
+        game: {
+            name: 'with Reseyn'
+        }
+    });
+});
+
+gbot.login(auth.token);
 
 export const bot = 5;
