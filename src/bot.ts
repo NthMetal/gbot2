d5fb6df3c90ebecff253bcfeeba065f6acdae8c2
nthmetal <832529@gmail.com>
2020-01-18 22:03:26 -0500

    CD

--- src/bot.ts
+++ src/bot.ts
@@ -6,13 +6,9 @@ import * as auth from '@config/auth.json';
 
 // Bootstraps GBot
 export const gbot = new GBot(new Client(), auth.token);
-// module.exports = new GBot(new Client(), auth.token);
 
 // Activates GBot
-
-if (auth.token) {
+if (auth.token && auth.token !== '<Discord Bot Token>') {
     gbot.events();
     gbot.login();
-
-    gbot.logout();
 }
